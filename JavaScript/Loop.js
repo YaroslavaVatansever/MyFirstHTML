c90c3b09c10248f9337d899d426a09c2326fe526
Yaroslava Semyda <yaskasemyda@gmail.com>
2024-07-20 14:48:31 -0400

    loops + videoHTML

--- JavaScript/Loop.js
+++ JavaScript/Loop.js
@@ -17,47 +17,120 @@
 // console.log(num2);
 
 
-const names = ["Chris", "Bob", "Joe"];
-let index = 0;
+
+// const names = ["Chris", "Bob", "Joe"];
+// let index = 0;
 
 
-while(index < names.length){
-    if (names[index] === "Joe"){
-        console.log(names[index]);
-        break
-    }
-    index++;
-}
-console.log(index);
-
-
-let shouldRunOnlyOnce = false;
-do{
-    console.log("Looping!");
-    shouldRunOnlyOnce = false;
-} while(shouldRunOnlyOnce);
-
-
-// let condition;
-// do {
-//     const userInput = promt("Enter q to exit");
-//     if(userInput === "q"){
-//         condition = false;
-//     } else {
-//         condition = true;
+// while(index < names.length){
+//     if (names[index] === "Joe"){
+//         console.log(names[index]);
+//         break
 //     }
-// } while(condition);
+//     index++;
+// }
+// console.log(index);
 
 
-let totalMoney = 100;
-do{
-    checkIfCanPurchase()
-} while(totalMoney > 500);
+// let shouldRunOnlyOnce = false;
+// do{
+//     console.log("Looping!");
+//     shouldRunOnlyOnce = false;
+// } while(shouldRunOnlyOnce);
 
-function checkIfCanPurchase(){
-    if(totalMoney > 500){
-        console.log("Can purchase!");
-    } else {
-        console.log("Cannot purchase...");
+
+// // let condition;
+// // do {
+// //     const userInput = promt("Enter q to exit");
+// //     if(userInput === "q"){
+// //         condition = false;
+// //     } else {
+// //         condition = true;
+// //     }
+// // } while(condition);
+
+
+// let totalMoney = 100;
+// do{
+//     checkIfCanPurchase()
+// } while(totalMoney > 500);
+
+// function checkIfCanPurchase(){
+//     if(totalMoney > 500){
+//         console.log("Can purchase!");
+//     } else {
+//         console.log("Cannot purchase...");
+//     } 
+// }
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+const iceCream = ["Vanilla", "Chocolate", "Cherry", "Lemon", "Oreo"];
+function IceCreamLooper(arr, type){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === type){
+            console.log(arr[i]+ " is at position: " + i);
+        }
     }
 }
+
+// IceCreamLooper(iceCream, "Cherry");
+
+// for(let i = 1; i <= 100; i++){
+//     if(i % 15 === 0){
+//         continue;
+
+//     } else if(i % 3 === 0){
+//         console.log("Fizz");
+//     } else if (i % 5 === 0){
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// let x = 0;
+// while(x < 10){
+    
+//     x++;
+//     if(x === 5){
+//         console.log("I'm skipped :(")
+//         continue;
+//     }
+//     console.log(x + ": I'm not being skipped :)");
+// }
+
+// outerLoop:
+// for (let i = 0; i < 10; i++){
+//     console.log("Loop #" + i);
+//     innerLoop:
+//     for(let a = 0; a < 10; a++){
+//         console.log("Inner loop #" + a);
+//         if(a > 4){
+//             break outerLoop;
+//         }
+//     }
+// }
+// console.log("Finished loop");
+
+// function looper(value){
+//     setTimeout(function(){
+//         console.log(value);
+//     }, 1000)
+// }
+
+// for(let i = 0; i < 10; i++){
+//     setTimeout(() =>{
+//         console.log(i);
+//     }, i * 500);
+// }
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i, len, text;
+for(i = 0, len = cars.length, text = ""; i < len; i++){
+    text += cars[i] + " ";
+}
+console.log(text);
--- JavaScript/SwichStatements.js
+++ JavaScript/SwichStatements.js
@@ -76,3 +76,32 @@ const donuts = (selection) => {
 }
 
 donuts("c");
+
+let day;
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+        text = "Looking forward to the Weekend!";
+  }
+
+  console.log("Today is " + day);
